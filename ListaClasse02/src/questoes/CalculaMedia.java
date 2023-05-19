abafd71d3ea509b2265b4d81398c6dd29fd8a6d0
marianakorb <74978652+marianakorb@users.noreply.github.com>
2023-05-19 15:55:05 -0300

    Update CalculaMedia.java

--- ListaClasse02/src/questoes/CalculaMedia.java
+++ ListaClasse02/src/questoes/CalculaMedia.java
@@ -8,82 +8,82 @@ public class CalculaMedia {
 	
 	// construtor sem parametros
 	public CalculaMedia() {
-		nota1 = 0;
-		nota2 = 0;
-		nota3 = 0;
-		System.out.println("Construtor sem parametro: " + nota1 + ", " + nota2 + ", " + nota3);
+		this.nota1 = 0;
+		this.nota2 = 0;
+		this.nota3 = 0;
+		System.out.println("Construtor sem parametro: " + this.nota1 + ", " + this.nota2 + ", " + this.nota3);
 	}
 	
 	// construtor com parametros
 	public CalculaMedia(double n1, double n2, double n3) {
 		if (n1 >= 0 && n1 <= 10 ) {
-			nota1 = n1;
+			this.nota1 = n1;
 		} else {
-			nota1 = 0;
+			this.nota1 = 0;
 		}
 		
 		if (n2 >= 0 && n2 <= 10 ) {
-			nota2 = n2;
+			this.nota2 = n2;
 		} else {
-			nota2 = 0;
+			this.nota2 = 0;
 		}
 		
 		if (n3 >= 0 && n3 <= 10 ) {
-			nota3 = n3;
+			this.nota3 = n3;
 		} else {
-			nota3 = 0;
+			this.nota3 = 0;
 		}
 		
-		System.out.println("Construtor com parametro: " + nota1 + ", " + nota2 + ", " + nota3);
+		System.out.println("Construtor com parametro: " + this.nota1 + ", " + this.nota2 + ", " + this.nota3);
 	}
 	
 	public void setNota1(double n1) {
 		if (n1 >= 0 && n1 <= 10 ) {
-			nota1 = n1;
+			this.nota1 = n1;
 		} else {
-			nota1 = 0;
+			this.nota1 = 0;
 		}
 		
-		System.out.println("nota1 set: " + nota1);
+		System.out.println("nota1 set: " + this.nota1);
 	}
 	
 	public void setNota2(double n2) {
 		if (n2 >= 0 && n2 <= 10 ) {
-			nota2 = n2;
+			this.nota2 = n2;
 		} else {
-			nota2 = 0;
+			this.nota2 = 0;
 		}
 		
-		System.out.println("nota2 set: " + nota2);
+		System.out.println("nota2 set: " + this.nota2);
 	}
 	
 	
 	public void setNota3(double n3) {
 		if (n3 >= 0 && n3 <= 10 ) {
-			nota3 = n3;
+			this.nota3 = n3;
 		} else {
-			nota3 = 0;
+			this.nota3 = 0;
 		}
 		
-		System.out.println("nota3 set: " + nota3);
+		System.out.println("nota3 set: " + this.nota3);
 	}
 	
 	public double getNota1() {
-		return nota1;
+		return this.nota1;
 	}
 	
 	public double getNota2() {
-		return nota2;
+		return this.nota2;
 	}
 	
 	public double getNota3() {
-		return nota3;
+		return this.nota3;
 	}
 	
 	
 	public void calculaMedia() {
 		double media;
-		media = (nota1 + nota2 + nota3) / 3;
+		media = (this.nota1 + this.nota2 + this.nota3) / 3;
 		
 		System.out.printf("Média: %.2f", media);
 	}
