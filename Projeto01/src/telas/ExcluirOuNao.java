218dae104ada173831e6f66ce5829defadc6ea12
marianakorb <74978652+marianakorb@users.noreply.github.com>
2023-06-13 21:22:44 -0300

    novo frame

---
+++ Projeto01/src/telas/ExcluirOuNao.java
@@ -0,0 +1,75 @@
+package telas;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ExcluirOuNao extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ExcluirOuNao dialog = new ExcluirOuNao();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ExcluirOuNao() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		setLocationRelativeTo(null);
+
+		
+		JLabel lblNewLabel = new JLabel("Certeza que deseja excluir esse produto do seu estoque?");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel.setBounds(25, 87, 385, 37);
+		contentPanel.add(lblNewLabel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton simButton = new JButton("Sim");
+				simButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// drop produto do estoque
+					}
+				});
+				simButton.setActionCommand("OK");
+				buttonPane.add(simButton);
+				getRootPane().setDefaultButton(simButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
